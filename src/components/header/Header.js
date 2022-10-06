36b07e4c2fa48fff672e71ae0487aacc7866a30e
LuisAndrade18 <felipeluis2002@gmail.com>
2022-10-06 18:47:47 -0300

    bug fixed to deploy

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Profile from './components/Profile';
 import Highlights from './components/Highlights';
-import SocialMedia from './components/SocialMedia';
 import * as S from './Style';
 
 const Header = () => {
